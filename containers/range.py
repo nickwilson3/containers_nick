9b994be60acadc6c6a0d3a80468fdf428b4e8f93
Nick Wilson <nicholas.wilson.23@cmc.edu>
2021-03-30 00:32:57 -0700

    update

--- containers/range.py
+++ containers/range.py
@@ -1,6 +1,6 @@
 def range(a, b=None, c=None):
     '''
-    This function should behave exactly like the built-in range function.
+    Ths function should behave exactly like the built-in range function.
     For example:
 
     >>> list(range(5))
